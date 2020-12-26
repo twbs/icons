5d6eb7e137aebacb26eadfded10a62805ee667c4
XhmikosR <xhmikosr@gmail.com>
2020-12-26 09:08:33 +0200

    build-svgs.js: switch to async/await

--- build/build-svgs.js
+++ build/build-svgs.js
@@ -23,79 +23,65 @@ const svgAttributes = {
 }
 
 const getSvgoConfig = async () => {
-  try {
-    let svgoConfig = await fs.readFile(path.join(__dirname, '../svgo.yml'), 'utf8')
+  const svgoConfigFile = await fs.readFile(path.join(__dirname, '../svgo.yml'), 'utf8')
 
-    svgoConfig = await yaml.safeLoad(svgoConfig)
+  return await yaml.safeLoad(svgoConfigFile)
+}
 
-    return svgoConfig
-  } catch (error) {
-    console.error('Couldn\'t read SVGO\'s config!')
-    console.error(error)
-    process.exit(1)
+const processFile = async (file, config) => {
+  const filepath = path.join(iconsDir, file)
+  const basename = path.basename(file, '.svg')
+
+  const originalSvg = await fs.readFile(filepath, 'utf8')
+  const svgo = await new SVGO(config)
+  const optimizedSvg = await svgo.optimize(originalSvg)
+
+  const $ = cheerio.load(optimizedSvg.data, {
+    xml: {
+      xmlMode: true
+    }
+  })
+  const $svgElement = $('svg')
+
+  // We keep all SVG contents apart from the `<svg>` element.
+  // `$(this)` refers to the original object not the replaced one!
+  $svgElement.replaceWith($('<svg>').append($(this).html()))
+
+  // Then we set the `svgAttributes` in the order we want to,
+  // hence why we remove the attributes and add them back
+  for (const [attribute, value] of Object.entries(svgAttributes)) {
+    $svgElement.removeAttr(attribute)
+    $svgElement.attr(attribute, attribute === 'class' ? `bi bi-${basename}` : value)
+  }
+
+  const resultSvg = $svgElement.toString().replace(/\r\n?/g, '\n')
+
+  await fs.writeFile(filepath, resultSvg, 'utf8')
+
+  if (VERBOSE) {
+    console.log(`- ${basename}`)
   }
 }
 
-const processFile = (file, config) => {
-  return new Promise((resolve, reject) => {
-    const filepath = path.join(iconsDir, file)
-    const basename = path.basename(file, '.svg')
+(async () => {
+  try {
+    const basename = path.basename(__filename)
+    const timeLabel = chalk.cyan(`[${basename}] finished`)
 
-    fs.readFile(filepath, 'utf8')
-      .then(data => {
-        const svgo = new SVGO(config)
+    console.log(chalk.cyan(`[${basename}] started`))
+    console.time(timeLabel)
 
-        svgo.optimize(data)
-          .then(result => {
-            const $ = cheerio.load(result.data, {
-              xml: {
-                xmlMode: true
-              }
-            })
-            const $svgElement = $('svg')
+    const files = await fs.readdir(iconsDir)
+    const config = await getSvgoConfig()
 
-            // We keep all SVG contents apart from the `<svg>` element.
-            // `$(this)` refers to the original object not the replaced one!
-            $svgElement.replaceWith($('<svg>').append($(this).html()))
+    await Promise.all(files.map(file => processFile(file, config)))
 
-            // Then we set the `svgAttributes` in the order we want to,
-            // hence why we remove the attributes and add them back
-            for (const [attribute, value] of Object.entries(svgAttributes)) {
-              $svgElement.removeAttr(attribute)
-              $svgElement.attr(attribute, attribute === 'class' ? `bi bi-${basename}` : value)
-            }
+    const filesLength = files.length
 
-            fs.writeFile(filepath, $svgElement.toString().replace(/\r\n?/g, '\n'), 'utf8')
-              .then(() => {
-                if (VERBOSE) {
-                  console.log(`- ${basename}`)
-                }
-                resolve()
-              })
-              .catch(error => reject(error))
-          })
-          .catch(error => reject(error))
-      })
-      .catch(error => reject(error))
-  })
-}
-
-const main = async () => {
-  const basename = path.basename(__filename)
-  const timeLabel = chalk.cyan(`[${basename}] finished`)
-
-  console.log(chalk.cyan(`[${basename}] started`))
-  console.time(timeLabel)
-
-  const files = await fs.readdir(iconsDir)
-  const config = await getSvgoConfig()
-
-  await Promise.all(files.map(file => processFile(file, config)))
-
-  const filesLength = files.length
-
-  console.log(chalk.green(`\nSuccess, ${filesLength} icon${filesLength !== 1 ? 's' : ''} prepped!`))
-  console.timeEnd(timeLabel)
-}
-
-main()
+    console.log(chalk.green(`\nSuccess, ${filesLength} icon${filesLength !== 1 ? 's' : ''} prepped!`))
+    console.timeEnd(timeLabel)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+})()
