9539bbed0bad1f6cf1704ac494d6d97222dc2dbc
XhmikosR <xhmikosr@gmail.com>
2021-12-24 15:33:02 +0200

    Update build-ts-type.js

--- build/build-ts-type.js
+++ build/build-ts-type.js
@@ -1,22 +1,17 @@
-"use strict"
+'use strict'
+
 const fs = require('fs')
 const ICON_PATH = fs.realpathSync('icons')
 const TYPE_PATH = fs.realpathSync('./')
 const TEMPLATE = 'export type BootstrapIcon ='
 
 if (fs.existsSync(ICON_PATH)) {
-
   let typeDef = TEMPLATE
-
 
   console.log('Start reading icons from ', ICON_PATH)
   fs.readdirSync(ICON_PATH)
-    .filter((folderItem) => {
-      return folderItem.match(/.svg$/)
-    })
-    .map((icon) => {
-      return `\n  | '${icon.replace('.svg', '')}'`
-    })
+    .filter((folderItem) => folderItem.match(/.svg$/))
+    .map((icon) => `\n  | '${icon.replace('.svg', '')}'`)
     .forEach((typeIconEntry) => {
       typeDef += typeIconEntry
     })
@@ -26,19 +21,12 @@ if (fs.existsSync(ICON_PATH)) {
 
     const typeFileDestination = `${TYPE_PATH}/bootstrap-icon.ts`;
 
-    fs.writeFileSync(typeFileDestination,
-      typeDef,
-      {
-        encoding: 'utf-8'
-      }
-    )
+    fs.writeFileSync(typeFileDestination, typeDef, { encoding: 'utf-8' })
 
     console.log(`Type for bootstrap-icons was created -> ${typeFileDestination}`)
-
   } else {
     console.log('An error occurred... Aboring.')
   }
-
 } else {
   console.log('Icon folder does not exist')
 }
