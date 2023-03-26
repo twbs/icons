128c153b9e1e2c69d6fa7269395423ac07052ea9
korki <65349313+korki43@users.noreply.github.com>
2023-03-26 19:09:17 +0300

    docs: filter url by search query

--- docs/assets/js/list.js
+++ docs/assets/js/list.js
@@ -3,7 +3,9 @@
 (function () {
   'use strict'
 
-  new List('icons-body', {
+  const searchInput = document.querySelector('#icons-body #search')
+  const query = new URLSearchParams(window.location.search).get('q')
+  const iconList = new List('icons-body', {
     searchDelay: 250,
     valueNames: [
       'name', {
@@ -13,5 +15,24 @@
         ]
       }
     ]
+  })
+
+  if (query) {
+    document.querySelector('#content').scrollIntoView()
+    searchInput.value = query
+    iconList.search(query)
+  }
+
+  iconList.on('searchComplete', () => {
+    const searchTerm = searchInput.value
+    const newUrl = new URL(window.location)
+
+    if (searchTerm.length > 0) {
+      newUrl.searchParams.set('q', searchTerm)
+    } else {
+      newUrl.searchParams.delete('q')
+    }
+
+    window.history.replaceState(null, null, newUrl)
   })
 })()
