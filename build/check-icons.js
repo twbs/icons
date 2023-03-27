f5621d468a5a1fb1ec14b9b10d86e9d8bfb21c19
korki <korki43@gmx.de>
2023-03-27 18:14:22 +0200

    check font json for differences with svgs (#1657)
    * check font json for differences with svgs
    
    * Split output
    
    * Rename and add script to npm scripts
    
    ---------
    
    Co-authored-by: XhmikosR <xhmikosr@gmail.com>

---
+++ build/check-icons.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const fs = require('node:fs').promises
+const path = require('node:path')
+const process = require('node:process')
+const picocolors = require('picocolors')
+
+const fontJson = require(path.join(__dirname, '../font/bootstrap-icons.json'))
+const iconsDir = path.join(__dirname, '../icons/')
+
+const jsonIconList = Object.keys(fontJson)
+
+;(async () => {
+  try {
+    const basename = path.basename(__filename)
+    const timeLabel = picocolors.cyan(`[${basename}] finished`)
+
+    console.log(picocolors.cyan(`[${basename}] started`))
+    console.time(timeLabel)
+
+    const files = await fs.readdir(iconsDir)
+    const svgIconList = files.map(file => file.slice(0, -4))
+
+    const onlyInJson = jsonIconList.filter(icon => !svgIconList.includes(icon))
+    const onlyInSvg = svgIconList.filter(icon => !jsonIconList.includes(icon))
+
+    if (onlyInJson.length !== 0 || onlyInSvg !== 0) {
+      if (onlyInJson.length > 0) {
+        console.error(picocolors.red('Found additional icons in JSON:'))
+
+        for (const icon of onlyInJson) {
+          console.log(`  - ${picocolors.red(icon)}`)
+        }
+
+        process.exit(1)
+      }
+
+      if (onlyInSvg.length > 0) {
+        console.error(picocolors.red('Found additional icons in SVG files:'))
+
+        for (const icon of onlyInSvg) {
+          console.log(`  - ${picocolors.red(icon)}`)
+        }
+
+        process.exit(1)
+      }
+    }
+
+    console.log(picocolors.green('Success, found no differences!'))
+    console.timeEnd(timeLabel)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+})()
